fd42d3bd9426b620db477dfd592639993f175bc6
zzk7908441 <zzk7908441@outlook.com>
2026-02-04 22:27:01 +0800

    Add files via upload

---
+++ 2月4日清洗数据练习.py
@@ -0,0 +1,20 @@
+raw_info = "  Agent:007_Bond; Coords:(40,74); Items:gun,money,gun; Mission:2025-RESCUE-X  "
+
+info = raw_info.strip()
+parts = info.split("; ")
+
+mission_id = parts[3][8:]
+coords = (40, 74)
+
+items_string = parts[2][6:]
+items_list = items_string.split(",")
+unique_items = set(items_list)
+
+agent_file = {
+    "Agent": "007_Bond",
+    "Mission_ID": mission_id,
+    "Location": coords,
+    "Equipment": list(unique_items)
+}
+
+print(agent_file)
