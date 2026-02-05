c1d6f52f9fa1cd37980eca253728b6374bcc4e2d
zzk7908441 <zzk7908441@outlook.com>
2026-02-05 16:50:30 +0800

    Update 2月4日清洗数据练习.py

--- 2月4日清洗数据练习.py
+++ 2月4日清洗数据练习.py
@@ -1,19 +1,15 @@
-raw_info = "  Agent:007_Bond; Coords:(40,74); Items:gun,money,gun; Mission:2025-RESCUE-X  "
-
-info = raw_info.strip()
-parts = info.split("; ")
-
-mission_id = parts[3][8:]
-coords = (40, 74)
-
-items_string = parts[2][6:]
-items_list = items_string.split(",")
-unique_items = set(items_list)
-
+raw_info = " Agent:007_Bond; Coords:(40,74); Items:gun,money,gun; \nMission:2025-RESCUE-X "
+clean_str = raw_info.strip().replace('\n', '')
+parts = [item.strip().split(':') for item in clean_str.split(';') if item.strip()]
+temp_map = {k.strip(): v.strip() for k, v in parts}
+items_set = set(temp_map['Items'].split(','))
+coords_tuple = tuple(map(int, temp_map['Coords'].replace('(', '').replace(')', '').split(',')))
+mission_code = temp_map['Mission'][5:]
 agent_file = {
-    "Agent": "007_Bond",
-    "Mission_ID": mission_id,
-    "Location": coords,
-    "Equipment": list(unique_items)}
+    'agent_id': temp_map['Agent'],
+    'coords': coords_tuple,
+    'unique_items': items_set,
+    'mission_code': mission_code
+}
 
 print(agent_file)
